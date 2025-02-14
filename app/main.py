c3a954025308e1b608f6d21c935cadbc31c8d1cf
Elijah Kimani <elijahkimani1293@gmail.com>
2025-02-14 12:02:18 +0300

    commit

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from .database import  Base
-from .routes import users, patients, appointments,auth  # Import your routes
+from .routes import auth  # Import your routes
 from app.database import sync_engine as engine, Base
 
 
@@ -10,9 +10,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI(title="Hospital Management System API")
 
 # Include routers
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(patients.router, prefix="/patients", tags=["Patients"])
-app.include_router(appointments.router, prefix="/appointments", tags=["Appointments"])
+
 app.include_router(auth.router)
 
 @app.get("/")
