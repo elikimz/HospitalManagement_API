442297ec1d960fd16747d070cebe2f48c1260dc1
Elijah Kimani <elijahkimani1293@gmail.com>
2025-02-14 12:25:41 +0300

    commit

--- app/main.py
+++ app/main.py
@@ -1,18 +1,23 @@
 from fastapi import FastAPI
-from .database import  Base
+from .database import async_engine, Base
 from .routes import auth  # Import your routes
-from app.database import sync_engine as engine, Base
-
 
 # Create database tables (if not using Alembic)
-Base.metadata.create_all(bind=engine)
+async def create_tables():
+    async with async_engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
 
 app = FastAPI(title="Hospital Management System API")
 
 # Include routers
-
 app.include_router(auth.router)
 
 @app.get("/")
 def root():
     return {"message": "Hospital Management API is running 🚀"}
+
+# Run table creation at startup
+import asyncio
+@app.on_event("startup")
+async def startup():
+    await create_tables()
